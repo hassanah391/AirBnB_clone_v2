7af44b97bf7cf4c37aa4f9ac31614731877d49fc
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-09 03:46:12 +0200

    Switches between two types of data storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python3
 '''comment'''
-from models.engine.file_storage import FileStorage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from os import getenv
 
-storage = FileStorage()
+type_storage = getenv("HBNB_TYPE_STORAGE")
+
+class_dict = {"BaseModel": BaseModel, "User": User, "State": State,
+                  "City": City, "Review": Review, "Amenity": Amenity,
+                  "Place": Place}
+
+if (type_storage == "db"):
+    from engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()

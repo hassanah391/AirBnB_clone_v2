3412d1326eccf56d89ef1b5ce2f16666961e092f
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-09 04:06:02 +0200

    Switches between two type of data storage

--- models/city.py
+++ models/city.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.state import State
+from os import getenv
 
 class City(BaseModel, Base):
     """A class that represents a city in Airbnb
@@ -16,6 +17,12 @@ class City(BaseModel, Base):
         name: string - empty string
     """
     __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=True)
-    name = Column(String(128), nullable=False)
-    # state = relationship("State", back_populates="cities")
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        # state = relationship("State", back_populates="cities")
+    
+    else:
+        state_id = ""
+        name = ""

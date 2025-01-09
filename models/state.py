832f2b4cc719eff5eff36db0de8855fee6f91580
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-09 03:42:48 +0200

    Maps State class onto its table in the db

--- models/state.py
+++ models/state.py
@@ -4,12 +4,31 @@
 Module state has a class called State that inherits from
 Basemodel class
 """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from models.city import City
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """A class that represents a state in Airbnb
     Public class attributes:
         name: string - empty string
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", 
+                        cascade="all, delete-orphan", 
+                        back_populates="state")
+    
+    @property
+    def cities(self):
+        '''
+            returns the list of City instances with state_id 
+            equals to the current State.id 
+        '''
+        list_cities = []
+        for city in models.storage.all("City").values():
+            if city.state_id == self.id:
+                list_cities.append(city)
+        return list_cities

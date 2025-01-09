5677080af7d0e5386069d9231a49bf5b69af8a59
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-09 03:42:18 +0200

    Maps City class onto its table in the db

--- models/city.py
+++ models/city.py
@@ -4,14 +4,18 @@
 Module city has a class called City that inherits from
 Basemodel class
 """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.state import State
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     """A class that represents a city in Airbnb
     Public class attributes:
         state_id: string - empty string: it will be the State.id
         name: string - empty string
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=True)
+    name = Column(String(128), nullable=False)
+    # state = relationship("State", back_populates="cities")

fd3dcdfaf15d7d081a1409156c903d4ddfdf1922
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-10 04:56:00 +0200

    Corrects the datatype of some class attributes and corrects the realtionships

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ Basemodel class
 """
 from models.base_model import BaseModel, Base
 from os import getenv
-from sqlalchemy import Column, INTEGER, String, ForeignKey, FLOAT
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 
 class Place(BaseModel, Base):
@@ -32,12 +32,14 @@ class Place(BaseModel, Base):
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
-        number_rooms = Column(INTEGER, nullable=False, default=0)
-        number_bathrooms = Column(INTEGER, nullable=False, default=0)
-        max_guest = Column(INTEGER, nullable=False, default=0)
-        price_by_night = Column(INTEGER, nullable=False, default=0)
-        latitude = Column(FLOAT, nullable=True)
-        longitude = Column(FLOAT, nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        user = relationship("User", back_populates="places")
+        cities = relationship("City", back_populates="places")
 
     else:
         city_id = ""

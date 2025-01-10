84d92f5320e885ca8e9345de0e9b5919bdb5567a
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-01-10 04:01:37 +0200

    Makes User class a maped class with its table

--- models/user.py
+++ models/user.py
@@ -5,11 +5,19 @@ Module user has a class called User
  that inherits from BaseModel
 """
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""

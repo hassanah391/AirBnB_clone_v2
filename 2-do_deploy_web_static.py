448656bb821a6feae5046ce367371092681274ca
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-07 21:00:00 +0200

    updates the script

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,13 @@ Module 2-do_deploy_web_static has
 a function do_deploy(archive_path):
 that distributes an archive to your web servers
 """
-from fabric.api import put, env, sudo
+from fabric.api import put, env, sudo, run
 import os.path
 
-# Define the list of host servers
+
 env.hosts = ['100.26.246.61', '54.237.93.3']
 env.user = 'ubuntu'
+
 
 def do_deploy(archive_path):
     """
@@ -45,9 +46,9 @@ def do_deploy(archive_path):
         if sudo("rm /tmp/{}".format(filename)).failed:
             return False
 
-        # Copy files to proper location with sudo (using cp -r instead of mv)
+        # Copy files to proper location with sudo
         if sudo("cp -r /data/web_static/releases/{}/web_static/* "
-               "/data/web_static/releases/{}/".format(name, name)).failed:
+                "/data/web_static/releases/{}/".format(name, name)).failed:
             return False
 
         # Remove the web_static directory
@@ -61,7 +62,48 @@ def do_deploy(archive_path):
 
         # Create new symbolic link
         if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-               .format(name)).failed:
+                .format(name)).failed:
+            return False
+
+        # Set proper permissions
+        if sudo("chown -R ubuntu:ubuntu /data/web_static/releases/{}".format(
+                name)).failed:
+            return False
+
+        # Add Nginx configuration
+        nginx_conf = (
+            "server {\n"
+            "    listen 80 default_server;\n"
+            "    listen [::]:80 default_server;\n"
+            "    add_header X-Served-By $hostname;\n"
+            "    root /var/www/html;\n"
+            "    index index.html index.htm;\n\n"
+            "    location /hbnb_static/ {\n"
+            "        alias /data/web_static/current/;\n"
+            "        index index.html index.htm;\n"
+            "    }\n"
+            "    location /redirect_me {\n"
+            "        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n"
+            "    }\n"
+            "    error_page 404 /404.html;\n"
+            "    location = /404.html {\n"
+            "        root /var/www/html;\n"
+            "        internal;\n"
+            "    }\n"
+            "}\n"
+        )
+
+        # Write the complete Nginx configuration
+        if sudo("echo '{}' > /etc/nginx/sites-available/default".format(
+                nginx_conf)).failed:
+            return False
+
+        # Test Nginx configuration
+        if sudo("nginx -t").failed:
+            return False
+
+        # Restart Nginx
+        if sudo("service nginx restart").failed:
             return False
 
         print("New version deployed!")

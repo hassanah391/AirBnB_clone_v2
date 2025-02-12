2635388167105aaf4d11e412490f83fb22093176
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-02-12 19:12:59 +0200

    Follows pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,6 @@ from models import *
 app = Flask(__name__)
 
 
-
 @app.route("/states_list", strict_slashes=False)
 def fetch_states():
     state_objs = [obj for obj in storage.all("State").values()]
@@ -14,6 +13,7 @@ def fetch_states():
         "7-states_list.html",
         state_objs=state_objs
         )
+
 
 @app.teardown_appcontext
 def tear_down(self):
